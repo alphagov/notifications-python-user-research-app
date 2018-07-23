1cc40ebab2b1f7b41c035fbfe379627fbd506615
Alexey Bezhan <al@fueledbylemons.com>
2018-07-23 14:12:49 +0100

    Update application.py for document upload test session

--- application.py
+++ application.py
@@ -3,32 +3,39 @@ from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 
 
+DOCUMENT_PATHS = {
+    'report': 'files/report.pdf',
+    'project-proposal': 'files/project-proposal.pdf',
+    'newsletter': 'files/newsletter.pdf',
+    'research-paper': 'files/research-paper.txt',
+}
+
+
 @app.route('/')
 def index():
-
-    phone_number = request.args.get('phone_number')
-
-    return render_template(
-        'index.html',
-        phone_number=phone_number
-    )
+    return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def send():
 
-    phone_number = request.form.get('phone_number')
+    email_address = request.form.get('email_address')
+    document_key = request.form.get('document_key')
 
     return redirect(url_for(
         '.sent',
-        phone_number=phone_number
+        email_address=email_address,
+        document_key=document_key
     ))
 
 
-@app.route('/sent/<phone_number>')
-def sent(phone_number=''):
+@app.route('/sent')
+def sent():
 
-    result = 'Phone number is {}'.format(phone_number)
+    email_address = request.args.get('email_address')
+    document_key = request.args.get('document_key')
+
+    result = 'Sent {} to {}'.format(DOCUMENT_PATHS[document_key], email_address)
 
     return render_template(
         'sent.html',
